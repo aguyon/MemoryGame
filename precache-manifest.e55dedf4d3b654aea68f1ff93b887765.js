d199519f33b764384063a5ed0631414c9b7337ca
aguyon <alexisguyon01@gmail.com>
2020-10-08 16:09:45 +0200

    Updates

---
+++ precache-manifest.e55dedf4d3b654aea68f1ff93b887765.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19d015f3b59c0faf20109ff9c08c720e",
+    "url": "/MemoryGame/index.html"
+  },
+  {
+    "revision": "4be886ffa9fd06bd4168",
+    "url": "/MemoryGame/static/css/main.f2aaa50e.chunk.css"
+  },
+  {
+    "revision": "3f730d31597d67ba9dfe",
+    "url": "/MemoryGame/static/js/2.707ed840.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/MemoryGame/static/js/2.707ed840.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4be886ffa9fd06bd4168",
+    "url": "/MemoryGame/static/js/main.578d15a5.chunk.js"
+  },
+  {
+    "revision": "07863e8972768bbffcad",
+    "url": "/MemoryGame/static/js/runtime-main.ff755aa8.js"
+  },
+  {
+    "revision": "0a2a3411b486bf1cdc7021ca6c19eca8",
+    "url": "/MemoryGame/static/media/apple.0a2a3411.png"
+  },
+  {
+    "revision": "56e8eabde5d8395e63aa118ea8ee933c",
+    "url": "/MemoryGame/static/media/avocado.56e8eabd.png"
+  },
+  {
+    "revision": "944da2e9ccc9873b8fe13efa83230e81",
+    "url": "/MemoryGame/static/media/banana.944da2e9.png"
+  },
+  {
+    "revision": "ac3dceecbaea0d1e9f2b94757b51ae13",
+    "url": "/MemoryGame/static/media/bravo.ac3dceec.gif"
+  },
+  {
+    "revision": "411d43250aa6e4977a20771395bd335d",
+    "url": "/MemoryGame/static/media/corn.411d4325.png"
+  },
+  {
+    "revision": "2c7096b5da77e3e076f1cdc6b88ac035",
+    "url": "/MemoryGame/static/media/lemon.2c7096b5.png"
+  },
+  {
+    "revision": "ba36bf4108f902637de346e3dff7ab5a",
+    "url": "/MemoryGame/static/media/lettuce.ba36bf41.png"
+  },
+  {
+    "revision": "78415daa6cce5c81170c377f1f90e7b6",
+    "url": "/MemoryGame/static/media/onion.78415daa.png"
+  },
+  {
+    "revision": "10c41a109647b6b3b4b2043ca7397a24",
+    "url": "/MemoryGame/static/media/strawberry.10c41a10.png"
+  }
+]);
